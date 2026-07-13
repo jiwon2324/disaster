e0661a06e40c0db018b99b2c87ac12433d8eb584
hyebinlee-qp <wofhdenddl04@naver.com>
2026-07-13 14:32:45 +0900

    DisasterBackApplication 이름 변경

--- disaster_back/src/main/java/com/disaster/api/DisasterBackApplication.java
+++ disaster_back/src/main/java/com/disaster/api/DisasterBackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebjjangBackApplication {
+public class DisasterBackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebjjangBackApplication.class, args);
+        SpringApplication.run(DisasterBackApplication.class, args);
     }
 
 }
