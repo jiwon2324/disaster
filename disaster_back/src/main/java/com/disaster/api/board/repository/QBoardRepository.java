5f75652600c5336ca7e08a8b5a2f55dfc162c8d1
kanghae05 <kanghae0510@gmail.com>
2026-07-14 14:13:20 +0900

    보드 이미지 지움

--- disaster_back/src/main/java/com/disaster/api/board/repository/QBoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.disaster.api.board.repository;
-
-import com.disaster.api.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface QBoardRepository
-extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
-}
--- disaster_back/src/main/java/com/disaster/api/image/repository/QImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.disaster.api.image.repository;
-
-import com.disaster.api.image.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface QImageRepository
-extends JpaRepository<Image, Long>, QuerydslPredicateExecutor<Image> {
-}
