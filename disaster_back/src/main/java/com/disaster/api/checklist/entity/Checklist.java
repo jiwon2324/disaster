73d3aa7db7701d672c00836d4358d0d6cf3a19cf
sarangee22 <rrangee22222@naver.com>
2026-07-14 14:20:35 +0900

    Update Checklist.java
    cheklist 엔티티 수정

--- disaster_back/src/main/java/com/disaster/api/checklist/entity/Checklist.java
+++ disaster_back/src/main/java/com/disaster/api/checklist/entity/Checklist.java
@@ -1,53 +1,42 @@
 package com.disaster.api.checklist.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "my_checklist")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "my_checklist")
 public class Checklist {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "no")
     private Long no;
 
-    @Column(name = "id", nullable = false, length = 20)
+    @Column(nullable = false, length = 20)
     private String id;
 
-    @Column(name = "name", nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(name = "category", length = 50)
+    @Column(length = 50)
     private String category;
 
-    @Builder.Default
     @Column(name = "is_ready", nullable = false, length = 1)
     private String isReady = "N";
 
-    @Column(name = "memo", length = 1000)
+    @Column(length = 1000)
     private String memo;
 
-    @Column(
-            name = "reg_date",
-            nullable = false,
-            updatable = false,
-            insertable = false
-    )
+    @CreatedDate
+    @Column(name = "reg_date", updatable = false)
     private LocalDateTime regDate;
-
-    @PrePersist
-    public void prePersist() {
-
-        if (this.isReady == null || this.isReady.isBlank()) {
-            this.isReady = "N";
-        }
-    }
 }
