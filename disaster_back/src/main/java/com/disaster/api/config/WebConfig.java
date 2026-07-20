f653236397426d56bc2cbd1af67f5c51eef85e50
sarangee22 <rrangee22222@naver.com>
2026-07-20 14:15:56 +0900

    Update WebConfig.java

--- disaster_back/src/main/java/com/disaster/api/config/WebConfig.java
+++ disaster_back/src/main/java/com/disaster/api/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.disaster.api.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,11 +13,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        log.info("[addResourceHandlers] image URI 폴더 추가 ------");
+        log.info("[addResourceHandlers] image URI ?대뜑 異붽? ------");
 
-        // URI로 접근이 안되는 폴더를 접근이 가능한 URI와 매칭시켜서 접근시켜준다.
+        // URI濡??묎렐???덈릺???대뜑瑜??묎렐??媛?ν븳 URI? 留ㅼ묶?쒖폒???묎렐?쒖폒以??
         registry.addResourceHandler("/upload/image/**")
                 .addResourceLocations("file:///C:/upload/image/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
