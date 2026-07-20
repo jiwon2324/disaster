189ac8532f775f59c7434190e76f2809012e0bd3
sarangee22 <rrangee22222@naver.com>
2026-07-20 14:16:06 +0900

    Update SecurityConfiguration.java

--- disaster_back/src/main/java/com/disaster/api/config/security/SecurityConfiguration.java
+++ disaster_back/src/main/java/com/disaster/api/config/security/SecurityConfiguration.java
@@ -15,43 +15,46 @@ public class SecurityConfiguration {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    // 생성자를 이용한 세팅
+    // ?앹꽦?먮? ?댁슜???명똿
     @Autowired
     public SecurityConfiguration(JwtTokenProvider jwtTokenProvider){
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @Bean // 리턴되는 객체(SecurityFilterChain)를 등록해서 사용한다.
+    @Bean // 由ы꽩?섎뒗 媛앹껜(SecurityFilterChain)瑜??깅줉?댁꽌 ?ъ슜?쒕떎.
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .csrf(AbstractHttpConfigurer::disable) //CSRF 보호 기능을 끄는 설정
+                .cors(cors -> {})
+                .csrf(AbstractHttpConfigurer::disable) //CSRF 蹂댄샇 湲곕뒫???꾨뒗 ?ㅼ젙
                 .sessionManagement(
                         httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS)) // session을 사용하지 않는다.
-                //사용자 이름(username)과 비밀번호(password)를 HTTP 헤더에 담아 보내는 가장 기본적인 인증 방식 사용안함.
-                // JWT으로 사용한다.
+                                SessionCreationPolicy.STATELESS)) // session???ъ슜?섏? ?딅뒗??
+                //?ъ슜???대쫫(username)怨?鍮꾨?踰덊샇(password)瑜?HTTP ?ㅻ뜑???댁븘 蹂대궡??媛??湲곕낯?곸씤 ?몄쬆 諛⑹떇 ?ъ슜?덊븿.
+                // JWT?쇰줈 ?ъ슜?쒕떎.
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                // 인증(Authentication) 없이 모든 사용자의 접근을 허용 - permitAll()
+                                // ?몄쬆(Authentication) ?놁씠 紐⑤뱺 ?ъ슜?먯쓽 ?묎렐???덉슜 - permitAll()
                                 .requestMatchers("/swagger",
                                 "/swagger-ui.html", "/swagger-ui/**", "/api-docs",
                                 "/api-docs/**", "/v3/api-docs/**" ).permitAll()
+                                .requestMatchers("/api/edu", "/api/edu/**",
+                                        "/api/checklists", "/api/checklists/**").permitAll()
                                 .requestMatchers("/member/login.do", "/member/write.do").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/product/**").permitAll()
                                 .requestMatchers("/board/**", "/image/**").permitAll()
                                 .requestMatchers("/upload/**").permitAll()
                                 .requestMatchers("/txt/**").permitAll()
                                 .requestMatchers("**exception**").permitAll()
-                                // 앞에서 정의한 URL을 제외한 모든 요청은 ADMIN 역할(Role)을 가진 사용자만
-                                // 접근할 수 있도록 하는 인가(Authorization) 규칙
-                                // 접근하는 사용자의 권한이 ADMIN인지 알기 위해서 토큰은 확인한다.
+                                // ?욎뿉???뺤쓽??URL???쒖쇅??紐⑤뱺 ?붿껌? ADMIN ??븷(Role)??媛吏??ъ슜?먮쭔
+                                // ?묎렐?????덈룄濡??섎뒗 ?멸?(Authorization) 洹쒖튃
+                                // ?묎렐?섎뒗 ?ъ슜?먯쓽 沅뚰븳??ADMIN?몄? ?뚭린 ?꾪빐???좏겙? ?뺤씤?쒕떎.
                                 .anyRequest().hasRole("ADMIN")
                         )
-                // 스프링 시큐리티에서 기본으로 제공하고 있는 로그임 폼을 비활성화한다.
+                // ?ㅽ봽留??쒗걧由ы떚?먯꽌 湲곕낯?쇰줈 ?쒓났?섍퀬 ?덈뒗 濡쒓렇???쇱쓣 鍮꾪솢?깊솕?쒕떎.
                 .formLogin(AbstractHttpConfigurer::disable)
-                // 토큰 처리를 앞에 다음 사용자 로그인(아이디, 비밀번호 확인 필터)
+                // ?좏겙 泥섎━瑜??욎뿉 ?ㅼ쓬 ?ъ슜??濡쒓렇???꾩씠?? 鍮꾨?踰덊샇 ?뺤씤 ?꾪꽣)
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling((exceptionHanling) ->
