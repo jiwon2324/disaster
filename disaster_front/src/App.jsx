3b0f22e904070e7c9836c2ae66648191b050fc78
sarangee22 <rrangee22222@naver.com>
2026-07-20 14:15:45 +0900

    Update App.jsx

--- disaster_front/src/App.jsx
+++ disaster_front/src/App.jsx
@@ -5,25 +5,37 @@ import NotFoundMenu from "./components/error/NotFoundMenu"
 import BoardComp from "./components/board/BoardComp"
 import ImageComp from "./components/image/ImageComp"
 import MemberComp from "./components/member/MemberComp"
+import EduGuideList from "./components/guide/EduGuideList"
+import EduGuideView from "./components/guide/EduGuideView"
+import EduGuideForm from "./components/guide/EduGuideForm"
+import ChecklistList from "./components/checklist/ChecklistList"
+import ChecklistForm from "./components/checklist/ChecklistForm"
 
 function App() {
 
   return (
     <>
-      {/* 맨 위에 메뉴 컴포넌트 : /src/components/common/TopNavi.jsx */}
+      {/* 留??꾩뿉 硫붾돱 而댄룷?뚰듃 : /src/components/common/TopNavi.jsx */}
       <TopNavi />
 
       <div className="container pt-5">
-      {/* 라이팅 - 메뉴별 */}
+      {/* ?쇱씠??- 硫붾돱蹂?*/}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/board/*" element={<BoardComp />} />
           <Route path="/image/*" element={<ImageComp />} />
           <Route path="/member/*" element={<MemberComp />} />
+          <Route path="/edu" element={<EduGuideList />} />
+          <Route path="/edu/write" element={<EduGuideForm />} />
+          <Route path="/edu/:no" element={<EduGuideView />} />
+          <Route path="/edu/:no/edit" element={<EduGuideForm />} />
+          <Route path="/checklists" element={<ChecklistList />} />
+          <Route path="/checklists/write" element={<ChecklistForm />} />
+          <Route path="/checklists/:no/edit" element={<ChecklistForm />} />
           <Route path="*" element={<NotFoundMenu />} />
         </Routes>
       </div>
-      {/* 맨 아래 회사 소개 & 카피라이트 : /src/components/common/Footer.jsx */}
+      {/* 留??꾨옒 ?뚯궗 ?뚭컻 & 移댄뵾?쇱씠??: /src/components/common/Footer.jsx */}
     </>
   )
 }
